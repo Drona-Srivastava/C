a4ac589333aa56777ec8a9c2594350a6fe0b0889
Drona Srivastava <srivastavadrona@gmail.com>
2024-04-30 12:48:42 +0530

    removed hackerrank question

--- Hackerrank.c
+++ Hackerrank.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-int main(){
-    int t;
-    scanf("%d",&t); //number of inputs
-    for(int a0 = 0; a0 < t; a0++){
-        int n;
-        scanf("%d",&n); //the max number
-        int sum = 0;
-        int three = (n - 1) / 3;
-        int five = (n - 1) / 5;
-        int fifteen = (n - 1) / 15;
-        sum = (3 * three * (three + 1) / 2) + (5 * five * (five + 1) / 2) - (15 * fifteen * (fifteen + 1) / 2);
-        printf("%d\n",sum);
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", &arr[i]);
     }
-    return 0;
+    for(int i=0, j=(num-1); i<num ,j>0; i++,j--){
+        arr[i] = arr[j];
+    }
+    for(int i=0; i<num ;i++){
+        printf("%d",*(arr+i));
+    }
 }
